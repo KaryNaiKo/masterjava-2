5f1a78e81774f4df661c16e464999599c5ae3f21
petr.voronin <petr.voronin@aisa.ru>
2020-07-15 13:33:06 +0300

    HW01 done

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,7 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -15,11 +17,45 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        final CompletionService<Result> completionService = new ExecutorCompletionService<>(executor);
+
+        for (int rowIndex = 0; rowIndex < matrixSize; rowIndex++) {
+            final int i = rowIndex;
+
+            completionService.submit(() -> {
+                int[] matrixCRow = new int[matrixSize];
+
+                for (int columnIndex = 0; columnIndex < matrixSize; columnIndex++) {
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += matrixA[i][k] * matrixB[k][columnIndex];
+                    }
+                    matrixCRow[columnIndex] = sum;
+                }
+
+                return new Result(i, matrixCRow);
+            });
+        }
+
+        for (int i = 0; i < matrixSize; i++) {
+            Result result = completionService.take().get();
+
+            int[] matrixCRow = result.getMatrixCRow();
+            for (int j = 0; j < matrixCRow.length; j++) {
+                matrixC[result.index][j] = matrixCRow[j];
+            }
+        }
+
         return matrixC;
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
+        //return withoutOptimization(matrixA, matrixB);
+        return optimized(matrixA, matrixB);
+    }
+
+    private static int[][] withoutOptimization(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
@@ -32,6 +68,33 @@ public class MatrixUtil {
                 matrixC[i][j] = sum;
             }
         }
+        return matrixC;
+    }
+
+    private static int[][] optimized(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        int thatColumn[] = new int[matrixSize];
+
+        try {
+            for (int j = 0; ; j++) {
+                for (int k = 0; k < matrixSize; k++) {
+                    thatColumn[k] = matrixB[k][j];
+                }
+
+                for (int i = 0; i < matrixSize; i++) {
+                    int thisRow[] = matrixA[i];
+                    int summand = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        summand += thisRow[k] * thatColumn[k];
+                    }
+                    matrixC[i][j] = summand;
+                }
+            }
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+
         return matrixC;
     }
 
@@ -57,5 +120,23 @@ public class MatrixUtil {
             }
         }
         return true;
+    }
+
+    static class Result {
+        private int index;
+        private int[] matrixCRow;
+
+        public Result(int index, int[] matrixCRow) {
+            this.index = index;
+            this.matrixCRow = matrixCRow;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int[] getMatrixCRow() {
+            return matrixCRow;
+        }
     }
 }
